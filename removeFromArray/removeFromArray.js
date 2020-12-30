1440d7ad38956c60cd852bce5cafba8336c24393
Chloe Urisohn <chloeurisohn@gmail.com>
2020-12-30 15:42:51 +0000

    arrays

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let newArr = [];
+    const array = args[0];
+    
+    array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArr.push(item);
+      };
+      });
 
-}
+      return newArr;
+};
 
 module.exports = removeFromArray
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+    if (min < 0 || max < 0) return 'ERROR';
+    if (min > max) {
+    const newNum = min;
+    min = max;
+    max = newNum;
+    };
+    
+    
+    let n = 0;
+    for (let i=min; i<max+1; i++) {
+       n += i;
+    }
+    return n;
 
-}
+};
+
 
 module.exports = sumAll
